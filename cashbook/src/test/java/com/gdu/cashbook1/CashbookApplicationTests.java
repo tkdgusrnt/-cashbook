be705628acc762060f5ba9b0b0443784a32f8789
GD7 <GD7@DESKTOP-QA6487N>
2020-05-12 11:20:20 +0900

    가게부 spring+maven+git 프로젝트 생성 초기화

---
+++ cashbook/src/test/java/com/gdu/cashbook1/CashbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdu.cashbook1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
